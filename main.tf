aeeb6dfcf96065b2655c6536c3620314932491b4
Simo.Tornioli <simone.tornioli@bain.com>
2024-02-01 10:08:24 +0100

    added destroy

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    bucket         = "terraform-bucket" # REPLACE WITH YOUR BUCKET NAME
+    key            = "terraform-ecs-app/terraform.tfstate"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
@@ -17,4 +23,35 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "terraform-bucket" # REPLACE WITH YOUR BUCKET NAME
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
