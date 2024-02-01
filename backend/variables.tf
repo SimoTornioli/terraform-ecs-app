7dda23427a9c30503ab1547a208de8165221fa25
Simo.Tornioli <simone.tornioli@bain.com>
2024-02-01 11:32:04 +0100

    new backend

---
+++ backend/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = null
+}
+
+variable "aws_access_id" {
+  sensitive = true
+  type      = string
+}
+
+variable "aws_secret_access_key" {
+  sensitive = true
+  type      = string
+}
--- main.tf
+++ main.tf
@@ -218,3 +218,36 @@ resource "aws_db_instance" "db_instance" {
   password                   = "foobarbaz"
   skip_final_snapshot        = true
 }
+
+
+## BACKEND
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "tf-bucket-state-simo" # REPLACE WITH YOUR BUCKET NAME
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
